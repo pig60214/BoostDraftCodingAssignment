2a3568edd10cf6481e60c0c0866157ecec26c0b9
YX <pig60214@gmail.com>
2024-04-07 19:16:07 -0400

    Fit Basic Tests

--- SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
+++ SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
@@ -5,6 +5,35 @@
         //Please implement this method
         public static bool DetermineXml(string xml)
         {
+            var stack = new Stack<string>();
+            var content = xml;
+            while (!string.IsNullOrEmpty(content))
+            {
+                var start = content.IndexOf("<", StringComparison.Ordinal);
+                var end = content.IndexOf(">", StringComparison.Ordinal);
+                var tag = GetTag(content, start, end);
+                if (stack.Count > 0)
+                {
+                    if (stack.Peek().Insert(1, "/") == tag)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(tag);
+                    }
+                }
+                else
+                {
+                    stack.Push(tag);
+                }
+                content = content.Substring(end + 1, content.Length - end - 1);
+            }
+            
+            return stack.Count == 0;
+        }
+        public static bool DetermineXml2(string xml)
+        {
             if (string.IsNullOrEmpty(xml)) return true;
             
             var startOfOpeningTag = xml.IndexOf("<", StringComparison.Ordinal);
@@ -25,7 +54,7 @@
             var closingTag = GetTag(xml, startOfClosingTag, endOfClosingTag);
             if (openingTag.Insert(1, "/") == closingTag)
             {
-                return DetermineXml(xml.Substring(endOfOpeningTag + 1, startOfClosingTag - endOfOpeningTag - 1));
+                return DetermineXml2(xml.Substring(endOfOpeningTag + 1, startOfClosingTag - endOfOpeningTag - 1));
             }
 
             return false;
