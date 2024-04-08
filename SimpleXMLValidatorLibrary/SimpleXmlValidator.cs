5ad59699a4f0c5919b4ec1cf08e48d1fc9acd66b
YX <pig60214@gmail.com>
2024-04-07 20:08:01 -0400

    Remove Recursive Version Code: Recursive Version Is Wrong

--- SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
+++ SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
@@ -73,34 +73,6 @@
             return openingTag.Insert(1, "/") == closingTag;
         }
 
-        public static bool DetermineXml2(string xml)
-        {
-            if (string.IsNullOrEmpty(xml)) return true;
-            
-            var startOfOpeningTag = xml.IndexOf("<", StringComparison.Ordinal);
-            var endOfOpeningTag = xml.IndexOf(">", StringComparison.Ordinal);
-            var startOfClosingTag = xml.LastIndexOf("<", StringComparison.Ordinal);
-            var endOfClosingTag = xml.LastIndexOf(">", StringComparison.Ordinal);
-            
-            if (startOfOpeningTag == -1 && endOfOpeningTag == -1 && startOfClosingTag == -1 && endOfClosingTag == -1)
-            {
-                return true;
-            }
-            if (startOfOpeningTag > 0 || endOfClosingTag != xml.Length - 1)
-            {
-                return false;
-            }
-            
-            var openingTag = GetTag(xml, startOfOpeningTag, endOfOpeningTag);
-            var closingTag = GetTag(xml, startOfClosingTag, endOfClosingTag);
-            if (openingTag.Insert(1, "/") == closingTag)
-            {
-                return DetermineXml2(xml.Substring(endOfOpeningTag + 1, startOfClosingTag - endOfOpeningTag - 1));
-            }
-
-            return false;
-        }
-
         private static string GetTag(string xml, int start, int end)
         {
             return xml.Substring(start, end - start + 1);
