d65126706de9a3875dbd269042700a1f5c7aeaf2
YX <pig60214@gmail.com>
2024-04-10 00:44:19 -0400

    Add Comments

--- SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
+++ SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
@@ -6,48 +6,73 @@ namespace SimpleXMLValidatorLibrary
         public static bool DetermineXml(string xml)
         {
             if (string.IsNullOrEmpty(xml)) return false;
-            var stack = new Stack<string>();
+            var stack = new Stack<string>(); // Store the tags that require their corresponding pair tags.
             var content = xml;
 
             var hasRootTag = false;
-
+    
             while (!string.IsNullOrEmpty(content))
             {
-                var start = content.IndexOf("<", StringComparison.Ordinal);
-                var end = content.IndexOf(">", StringComparison.Ordinal);
-                if (start == -1 && end == -1)
+                // Find the indices of left and right angle brackets.
+                var leftBracket = content.IndexOf("<", StringComparison.Ordinal);
+                var rightBracket = content.IndexOf(">", StringComparison.Ordinal);
+                
+                // If no left and right brackets are found, means there is already no tag in the content.
+                // So break the loop.
+                if (leftBracket == -1 && rightBracket == -1)
                 {
                     break;
                 }
-                if (end < start || start == -1 || end == -1)
+                
+                // If right bracket appears before left bracket(like "><")
+                // or left bracket is not found(like ">")
+                // or right bracket is not found(like "<"),
+                // it means the bracket(s) can't form a tag.
+                // So return false.
+                if (rightBracket < leftBracket || leftBracket == -1 || rightBracket == -1)
                 {
                     return false;
                 }
-                var tag = GetTag(content, start, end);
+                
+                // Extract the tag from content by the indices.
+                var tag = GetTag(content, leftBracket, rightBracket);
+                
+                // Check if stack is not empty
+                // It means there are still tags awaiting their pair tags.
                 if (stack.Count > 0)
                 {
+                    // If current tag pairs with the top tag in stack, means the top tag finds its pair tag.
+                    // So remove the top tag from stack.
                     if (DeterminePairTags(stack.Peek(), tag))
                     {
                         stack.Pop();
                     }
                     else
                     {
+                        // If not paired, means this tag also requires its corresponding pair tag.
+                        // So push the current tag onto the stack.
                         stack.Push(tag);
                     }
                 }
                 else
                 {
+                    // If stack is empty, it means current tag is a root tag.
+                    // XML doesn't allow two root tags.
+                    // So, if a root tag has already been encountered, this content is invalid and return false.
                     if (hasRootTag)
                     {
                         return false;
                     }
 
+                    // Set hasRootTag to true to indicate that a root tag has been encountered.
                     hasRootTag = true;
                     stack.Push(tag);
                 }
-                content = content.Substring(end + 1, content.Length - end - 1);
+                // Remove the processed tag from the content.
+                content = content.Substring(rightBracket + 1, content.Length - rightBracket - 1);
             }
             
+            // Check If the xml has root tag and if all tags have their pair tags.
             return hasRootTag && stack.Count == 0;
         }
 
